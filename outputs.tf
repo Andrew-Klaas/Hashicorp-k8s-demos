62709b34752841b7ccb3140014c2aaa3b87ace8d
Andrew Klaas <aklaas@hashicorp.com>
2021-01-04 12:52:36 -0600

    blah

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "k8s_endpoint" {
 }
 
 output "gcloud_connect_command" {
-  value = "gcloud container clusters get-credentials var.cluster_name --zone var.gcp_zone --project var.gcp_project"
+  value = "gcloud container clusters get-credentials ${var.cluster_name} --zone ${var.gcp_zone} --project ${var.gcp_project}"
 }
